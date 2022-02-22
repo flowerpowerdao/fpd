4c3f6ff2ce551aa8eb1d67ccceca9257e1ff7ac2
Tarek <trkmos@gmail.com>
2022-02-22 21:09:18 +0100

    run prettier

--- src/frontend/utils.ts
+++ src/frontend/utils.ts
@@ -1,28 +1,28 @@
 // Results
 export function fromOk(result): any {
-    // get the value from an ok result
-    return result.ok;
+  // get the value from an ok result
+  return result.ok;
 }
 
 export function fromErr(result): any {
-    // get the err from an error result
-    return Object.keys(result.err)[0];
+  // get the err from an error result
+  return Object.keys(result.err)[0];
 }
 
 export function isOk(result): boolean {
-    if (result) {
-        if ("ok" in result) {
-            return true;
-        }
+  if (result) {
+    if ("ok" in result) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 export function isErr(result): boolean {
-    if (result) {
-        if ("err" in result) {
-            return true;
-        }
+  if (result) {
+    if ("err" in result) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
