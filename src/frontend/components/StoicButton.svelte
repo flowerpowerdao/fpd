4f740322b46c127077f7aa58ac4d0bdaa58dffe3
Tarek <trkmos@gmail.com>
2022-02-24 08:17:33 +0100

    ✨ add stoic login

---
+++ src/frontend/components/StoicButton.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from "svelte";
+
+  import { store } from "../store";
+</script>
+
+{#if $store.isAuthed === "stoic"}
+  <button
+    class="flex items-center px-3 py-2 rounded-lg bg-white border-gray-300 border-2 hover:ring-2 hover:ring-opacity-50 hover:ring-indigo-500 hover:border-indigo-500"
+    on:click={store.disconnect}
+  >
+    <img
+      src="https://raw.githubusercontent.com/FloorLamp/cubic/main/src/ui/public/img/stoic.png"
+      class="w-4 mr-2"
+      alt="stoic logo"
+    />
+
+    <span>
+      {$store.principal.toString().slice(0, 5) +
+        "..." +
+        $store.principal.toString().slice(-5)}
+    </span>
+  </button>
+{:else if !$store.isAuthed}
+  <button
+    class="flex items-center px-3 py-2 rounded-lg bg-white border-gray-300 border-2 hover:ring-2 hover:ring-opacity-50 hover:ring-indigo-500 hover:border-indigo-500"
+    on:click={store.stoicConnect}
+  >
+    <img
+      src="https://raw.githubusercontent.com/FloorLamp/cubic/main/src/ui/public/img/stoic.png"
+      class="w-4 mr-2"
+      alt="stoic logo"
+    /> Stoic
+  </button>
+{/if}
