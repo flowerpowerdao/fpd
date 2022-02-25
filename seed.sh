71539914c98efe1ae0f37b9049b99802296f6861
Moritz <fullermoritz@gmail.com>
2022-02-25 16:11:42 +0100

    ✨ added shell script to seed dao canister with proposals

--- seed.sh
+++ seed.sh
@@ -1,9 +1,4 @@
-#!/usr/local/bin/ic-repl -r local
-// assume we already installed the dao canister
-import dao = "ryjl3-tyaaa-aaaaa-aaaba-cai";
-call dao.whoami();
-let result = _;
-assert _ == "2vxsx-fae";
-identity mainnet;
-call dao.whoami();
-assert _ == "cbvco-k27pa-dgumq-tjhcq-iqrcx-ayr3z-moywz-jqblc-nvsif-dayv3-4qe";
+for i in {0..30}
+do
+dfx canister call --async dao submit_proposal '("proposal '$i'", vec { "option 1"; "option 2"; "option 3"})'
+done
