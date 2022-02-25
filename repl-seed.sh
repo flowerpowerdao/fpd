c212a8b18c720a899b07b9b0311037222b553aff
Moritz <fullermoritz@gmail.com>
2022-02-25 17:42:52 +0100

    🔥 deleted seed-repl

--- repl-seed.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/ic-repl -r local
-// assume we already installed the dao canister
-import dao = "ryjl3-tyaaa-aaaaa-aaaba-cai";
-call dao.whoami();
-let result = _;
-assert _ == "2vxsx-fae";
-identity mainnet;
-call dao.whoami();
-assert _ == "cbvco-k27pa-dgumq-tjhcq-iqrcx-ayr3z-moywz-jqblc-nvsif-dayv3-4qe";
