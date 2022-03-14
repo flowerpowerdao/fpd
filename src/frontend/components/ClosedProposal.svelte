1cbd3079220eea3f7f63d6e26129468bb6c2d002
Moritz <fullermoritz@gmail.com>
2022-03-14 13:17:15 +0100

    🩹 removed styling for closed proposal

--- src/frontend/components/ClosedProposal.svelte
+++ src/frontend/components/ClosedProposal.svelte
@@ -6,9 +6,6 @@
   export let proposal: ProposalOverviewType;
 </script>
 
-<div
-  class="flex flex-col m-5 rounded-3xl text-center bg-gray-400 hover:bg-gray-700 cursor-pointer "
-  on:click={() => push(`/proposal/${proposal.id}`)}
->
+<div class="bg-white shadow overflow-hidden sm:rounded-md">
   <ProposalOverview {proposal} />
 </div>
