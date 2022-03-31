6b3c3363d7403dbdc4f16462f89d0492fed02520
Moritz <fullermoritz@gmail.com>
2022-03-31 17:37:54 +0200

    ✨ use proposal overview for voting history

--- src/frontend/components/ProposalOverview.svelte
+++ src/frontend/components/ProposalOverview.svelte
@@ -2,7 +2,7 @@
   import { push } from "svelte-spa-router";
   import type { ProposalView as Proposal } from "../../declarations/dao/dao.did";
   import { store } from "../store";
-  import { fromTimestamp } from "../utils";
+  import { fromTimestamp, fromVariantToString } from "../utils";
 
   export let proposal: Proposal;
   $: alreadyVoted = $store.votingHistory.includes(proposal.id);
@@ -30,28 +30,50 @@
             <div class="mt-2 flex">
               <div class="flex items-center text-sm text-gray-500">
                 <!-- Heroicon name: solid/calendar -->
-                <svg
-                  class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <p>
-                  Expiry Date
-                  <time
-                    datetime={fromTimestamp(
-                      proposal.expiryDate,
-                    ).toLocaleString()}
-                    >{fromTimestamp(proposal.expiryDate).toLocaleString()}</time
+                {#if fromVariantToString(proposal.state) === "open"}
+                  <svg
+                    class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
                   >
-                </p>
+                    <path
+                      fill-rule="evenodd"
+                      d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <p>
+                    Expiry Date
+                    <time
+                      datetime={fromTimestamp(
+                        proposal.expiryDate,
+                      ).toLocaleString()}
+                      >{fromTimestamp(
+                        proposal.expiryDate,
+                      ).toLocaleString()}</time
+                    >
+                  </p>
+                {:else}
+                  <div class="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
+                    <div class="ml-2 flex-shrink-0 flex">
+                      {#if fromVariantToString(proposal.state) === "rejected"}
+                        <p
+                          class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800"
+                        >
+                          rejected
+                        </p>
+                      {:else}
+                        <p
+                          class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                        >
+                          adopted
+                        </p>
+                      {/if}
+                    </div>
+                  </div>
+                {/if}
               </div>
             </div>
           </div>
@@ -62,7 +84,7 @@
               <p
                 class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
               >
-                Voted
+                voted
               </p>
             </div>
           </div>
