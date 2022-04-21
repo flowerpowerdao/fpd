db1eee469c24c5b2b66ade579b4a531317e753f1
Moritz <fullermoritz@gmail.com>
2022-04-21 12:44:52 +0200

    ✨ increase button height to 12

--- src/frontend/components/DarkMode.svelte
+++ src/frontend/components/DarkMode.svelte
@@ -6,6 +6,6 @@
 
 <button
   on:click={toggleDarkMode}
-  class="font-mono text-3xl bg-white dark:bg-black  border-black dark:border-white dark:text-white border-2 w-14 h-10 rounded-3xl"
+  class="font-mono text-3xl bg-white dark:bg-black  border-black dark:border-white dark:text-white border-2 w-14 h-12 rounded-3xl"
   >⁂</button
 >
--- src/frontend/components/Login.svelte
+++ src/frontend/components/Login.svelte
@@ -6,6 +6,6 @@
 
 <button
   on:click={toggleDarkMode}
-  class="flex-1 font-mono text-xl bg-white dark:bg-black border-black dark:border-white dark:text-white border-2 h-10 rounded-t-3xl"
+  class="flex-1 font-mono text-xl bg-white dark:bg-black border-black dark:border-white dark:text-white border-2 h-12 rounded-t-3xl"
   >login</button
 >
