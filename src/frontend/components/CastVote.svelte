b9d2375e2e7ed9851c9c0162de61e3c0e4a41d17
Moritz <fullermoritz@gmail.com>
2022-04-27 20:35:47 +0200

    ✨ added cast vote indicator

--- src/frontend/components/CastVote.svelte
+++ src/frontend/components/CastVote.svelte
@@ -11,7 +11,9 @@
   let selected;
   export let proposal: Proposal;
 
-  $: alreadyVoted = $store.votingHistory.includes(proposal.id);
+  $: alreadyVoted = $store.votingHistory
+    .map((element) => element.id)
+    .includes(proposal.id);
 
   function toggleModal() {
     openModal = !openModal;
@@ -34,15 +36,20 @@
 
 {#if fromVariantToString(proposal.state) === "open"}
   <Card>
+    {@const vote = $store.votingHistory.find(
+      (element) => Number(element.id) === Number(proposal.id),
+    )}
     <div class="p-2 flex flex-col justify-between min-h-[360px]">
       <h1 class="font-everett-medium text-3xl">cast your vote</h1>
       <div class="flex flex-col justify-center gap-2">
         {#each proposal.options as option, index}
           <button
-            class="{index === selected
-              ? 'shadow'
-              : ''} text-xl bg-white dark:bg-black border-2 border-black dark:border-white dark:text-white rounded-3xl h-12 w-full font-mono"
+            class="
+              {index === selected ? 'shadow' : ''} 
+              {Number(vote?.option) === index ? 'shadow' : ''}
+              text-xl bg-white dark:bg-black border-2 border-black dark:border-white dark:text-white rounded-3xl h-12 w-full font-mono"
             on:click={() => (selected = index)}
+            disabled={alreadyVoted}
           >
             {truncate(option, 20)}
           </button>
