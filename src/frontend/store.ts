db0429e1390d612b3e9fc7d0fa30f97d1243eb1b
Moritz <fullermoritz@gmail.com>
2022-05-04 23:50:10 +0200

    🩹 added canister ids back so frontend can determine them correctly in production

--- src/frontend/store.ts
+++ src/frontend/store.ts
@@ -85,6 +85,9 @@ export const createStore = ({
 }) => {
   const { subscribe, update } = writable<State>(defaultState);
 
+  console.log(ethflowerCanisterId);
+  console.log(btcflowerCanisterId);
+
   const stoicConnect = () => {
     StoicIdentity.load().then(async (identity) => {
       if (identity !== false) {
