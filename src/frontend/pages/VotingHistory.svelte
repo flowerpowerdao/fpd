17b96b8cb338a3e2520585db7cf93f4a25a81d15
Moritz <fullermoritz@gmail.com>
2022-05-04 13:49:02 +0200

    ✨ added desktop voting history

--- src/frontend/pages/VotingHistory.svelte
+++ src/frontend/pages/VotingHistory.svelte
@@ -1,64 +1,24 @@
 <script lang="ts">
-  import { store } from "../store";
-  import { pop, push } from "svelte-spa-router";
-  import spinner from "../assets/loading.gif";
-
+  import VotingHistoryTable from "../components/VotingHistoryTable.svelte";
   import Button from "../components/Button.svelte";
-  import Card from "../components/Card.svelte";
+
+  import { pop } from "svelte-spa-router";
 </script>
 
 <!-- mobile -->
-<div class="pb-24">
+<div class="pb-24 lg:hidden">
   <!-- header buttons -->
   <div class="my-10">
     <Button eventHandler={() => pop()}>← back</Button>
   </div>
-  {#if !$store.isAuthed}
-    <p class="mt-14 text-2xl">
-      You need to be logged in to see your voting history ¯\_(ツ)_/¯
-    </p>
-  {:else if $store.isLoading}
-    <img src={spinner} alt="loading spinner" />
-  {:else if $store.votingHistory.length === 0}
-    <p class="mt-14 text-2xl">You haven't voted on any proposals yet 🥺</p>
-  {:else}
-    <!-- voting history -->
-    <Card>
-      <div class="p-2 flex flex-col">
-        <h1 class="font-everett-medium text-3xl">voting history:</h1>
-        <table class="mt-4">
-          <thead>
-            <tr class="font-mono">
-              <th class="text-left">id</th>
-              <th class="text-left">titel</th>
-              <th class="text-right">option</th>
-            </tr>
-          </thead>
-          <tbody>
-            {#each $store.votingHistory as vote}
-              {@const proposal = $store.proposals.find(
-                (element) => Number(element.id) === Number(vote.id),
-              )}
-              <!-- this is needed, otherwise votingHistory might be fetched before
-              proposals and proposal.title is undefined -->
-              {#if proposal}
-                <tr
-                  class="cursor-pointer"
-                  on:click={() => push(`/proposals/${Number(vote.id)}`)}
-                >
-                  <!-- max-w-0 is needed for truncate to work correctly 
-                  https://stackoverflow.com/questions/9789723/css-text-overflow-in-a-table-cell -->
-                  <td class="truncate max-w-0">{vote.id}</td>
-                  <td class="truncate max-w-0">{proposal.title}</td>
-                  <td class="text-right truncate max-w-0"
-                    >{proposal.options[Number(vote.option)]}</td
-                  >
-                </tr>
-              {/if}
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </Card>
-  {/if}
+  <VotingHistoryTable />
+</div>
+
+<!-- desktop -->
+<div class="hidden lg:flex pb-24 pt-40">
+  <Button style={"max-w-[150px]"} eventHandler={() => pop()}>← back</Button>
+  <div class="flex flex-col w-full gap-4">
+    <VotingHistoryTable />
+  </div>
+  <div class="2xl:min-w-[430px] min-w-[350px]" />
 </div>
