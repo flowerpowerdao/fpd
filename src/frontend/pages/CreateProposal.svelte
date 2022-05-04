eae3c33fa553eba2b2343d5aad9c94f94547d7cf
Moritz <fullermoritz@gmail.com>
2022-05-04 13:41:42 +0200

    ✨ added create proposals page for desktop

--- src/frontend/pages/CreateProposal.svelte
+++ src/frontend/pages/CreateProposal.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
+  import Button from "../components/Button.svelte";
   import CreateProposalForm from "../components/CreateProposalForm.svelte";
+
+  import { pop } from "svelte-spa-router";
+
+  let preview = false;
 </script>
 
-<CreateProposalForm />
+<!-- mobile -->
+<div class="pb-24 lg:hidden">
+  <!-- header buttons -->
+  <div class="my-10">
+    <Button eventHandler={() => pop()}>← back</Button>
+    <Button eventHandler={() => (preview = !preview)}>preview</Button>
+  </div>
+  <CreateProposalForm {preview} />
+</div>
+
+<!-- desktop -->
+<div class="hidden lg:flex pb-24 pt-40">
+  <Button style={"max-w-[150px]"} eventHandler={() => pop()}>← back</Button>
+  <div class="flex flex-col w-full gap-4">
+    <CreateProposalForm {preview} />
+  </div>
+  <div class="2xl:min-w-[430px] min-w-[350px]">
+    <Button eventHandler={() => (preview = !preview)}>preview</Button>
+  </div>
+</div>
